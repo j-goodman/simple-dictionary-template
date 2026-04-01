825c7bc3bf3d3f6542310238565012ac090a652a
j-goodman <johncurtisgoodman@gmail.com>
2026-03-31 20:41:44 -0400

    Add event listener event on button click.

--- simple-dictionary.js
+++ simple-dictionary.js
@@ -1,3 +1,11 @@
 getDefinitionAndThen("cantaloupe", definition => {
     console.log(definition[0].meanings[0].definitions[0].definition)
 })
+
+const inputBox = document.getElementById("input-box")
+const submitButton = document.getElementById("submit-button")
+const definitionBox = document.getElementById("definition-box")
+
+submitButton.addEventListener("click", () => {
+    console.log("The button has been clicked!")
+})
